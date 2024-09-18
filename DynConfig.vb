0f5fbbdfc3cf82cbbc910179913aa2a77f7a459c
naomai <lulu@nieznani-sprawcy.pl>
2024-09-18 12:02:38 +0200

    add support for namespaces in DynConfig

--- DynConfig.vb
+++ DynConfig.vb
@@ -4,19 +4,23 @@ Imports System.Threading
 
 Public Class DynConfig
     Dim dbCtx As Utt2Context
-    Public Sub New(context As Utt2Context)
+    Protected nsName As String
+    Public Sub New(context As Utt2Context, Optional ns As String = "")
         dbCtx = context
+        nsName = ns
     End Sub
     Public Function getProperty(key As String)
-        Return dbCtx.ConfigProps.SingleOrDefault(Function(p) p.Key = key).Data
+        Dim keyFull = GetFullyQualifiedName(key)
+        Return dbCtx.ConfigProps.SingleOrDefault(Function(p) p.Key = keyFull).Data
     End Function
 
     Public Sub setProperty(key As String, data As String, Optional priv As Boolean = False)
-        Dim prop As ConfigProp = dbCtx.ConfigProps.SingleOrDefault(Function(p) p.Key = key)
+        Dim keyFull = GetFullyQualifiedName(key)
+        Dim prop As ConfigProp = dbCtx.ConfigProps.SingleOrDefault(Function(p) p.Key = keyFull)
 
         If IsNothing(prop) Then
             prop = New ConfigProp With {
-                .Key = key
+                .Key = keyFull
             }
         End If
 
@@ -27,13 +31,21 @@ Public Class DynConfig
     End Sub
 
     Public Sub unsetProperty(key As String)
-        Dim keyGroupPrefix As String = key + ".%"
+        Dim keyFull = GetFullyQualifiedName(key)
+        Dim keyGroupPrefix As String = keyFull + ".%"
         Dim propAffected = dbCtx.ConfigProps.Where(
-            Function(p) p.Key = key OrElse EF.Functions.Like(p.Key, keyGroupPrefix)
+            Function(p) p.Key = keyFull OrElse EF.Functions.Like(p.Key, keyGroupPrefix)
             ).ToList()
 
         dbCtx.ConfigProps.RemoveRange(propAffected)
         dbCtx.SaveChanges()
     End Sub
+
+    Protected Function GetFullyQualifiedName(key As String)
+        If nsName = "" Then
+            Return key
+        End If
+        Return nsName + "." + key
+    End Function
 
 End Class
