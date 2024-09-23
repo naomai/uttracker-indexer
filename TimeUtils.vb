ebf6c51aa1999b7922abb8afdd06690ad3f5052e
naomai <lulu@nieznani-sprawcy.pl>
2024-09-23 11:39:43 +0200

    make UnixTimestamp a time utility module

--- TimeUtils.vb
+++ TimeUtils.vb
@@ -1,10 +1,14 @@
-﻿Module UnixTimestamp
-    Public Function unixTime(Optional timestamp As DateTime = Nothing) As UInt64
+﻿Module TimeUtils
+    Public Function UnixTime(Optional timestamp As DateTime = Nothing) As UInt64
         Static epochStart As DateTime = New DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)
         If timestamp = Nothing Then
             timestamp = DateTime.UtcNow
         End If
 
         Return (timestamp - epochStart).TotalSeconds
+    End Function
+
+    Public Function TickCount()
+        Return Environment.TickCount And Int32.MaxValue
     End Function
 End Module
