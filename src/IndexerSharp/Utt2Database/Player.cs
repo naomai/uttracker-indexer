1973f03040933c1146e7fbf172d4cad1ed495073
naomai <lulu@nieznani-sprawcy.pl>
2025-02-05 14:24:11 +0100

    fix: correct entity def to prevent db locking

--- src/IndexerSharp/Utt2Database/Player.cs
+++ src/IndexerSharp/Utt2Database/Player.cs
@@ -5,7 +5,7 @@ public partial class Player
     public string Slug { get; set; } = null!;
     public string Name { get; set; } = null!;
     public string SkinData { get; set; } = null!;
-    public string Country { get; set; } = null!;
+    public string? Country { get; set; } = null!;
     public ICollection<PlayerLog> PlayerLogs { get; set; } = new List<PlayerLog>();
     public ICollection<PlayerStat> PlayerStats { get; set; } = new List<PlayerStat>();
 }
