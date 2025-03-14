3d460f45e3bef129210dab985ff14cb20cd1d1e3
naomai <lulu@nieznani-sprawcy.pl>
2025-03-14 12:56:06 +0100

    refactor: break down Main function

--- src/App.vb
+++ src/App.vb
@@ -16,24 +16,40 @@ Module App
     Dim dyncfg As IPropsProvider
 
     Sub Main()
-        Dim appName = System.Reflection.Assembly.GetEntryAssembly.GetName.Name
-        ini = GetIniConfig()
-        log = New Logger()
-
-        log.consoleLoggingLevel = (LoggerLevel.err Or LoggerLevel.out Or LoggerLevel.debug)
-        If ini.GetProperty("General.LogToFile", 0) Then
-            log.fileLoggingLevel = (LoggerLevel.err Or LoggerLevel.out Or LoggerLevel.debug)
-        Else
-            log.fileLoggingLevel = 0
-        End If
-
+        ini = GetAppIniProvider()
+        log = CreateLogger(ini)
 
         log.ErrorWriteLine("UTTracker Scanner")
         log.ErrorWriteLine("2009-24 naomai")
         log.ErrorWriteLine("")
         log.ErrorWriteLine("Loading config file from: {0}", ini.IniName)
 
+        Dim dbconfig As MySQLDBConfig = GetDbConfig(ini)
+        dbCtx = New Utt2Context(dbconfig)
 
+        Dim dyncfgDbCtx = New Utt2Context(dbconfig)
+        dyncfg = New DatabasePropsProvider(dyncfgDbCtx).Ns("utt.reaper")
+        dyncfg.SetProperty("configsrc", ini.IniName, True)
+
+        InitMasterServerManager(log, ini)
+        InitScanner(log, dbCtx, masterManager)
+        InitGSMasterServer(dbconfig, ini)
+
+        scanner.ScannerThread()
+    End Sub
+
+    Private Function CreateLogger(configSource As IniPropsProvider) As Logger
+        Dim log = New Logger()
+        log.consoleLoggingLevel = (LoggerLevel.err Or LoggerLevel.out Or LoggerLevel.debug)
+        If configSource.GetProperty("General.LogToFile", 0) Then
+            log.fileLoggingLevel = (LoggerLevel.err Or LoggerLevel.out Or LoggerLevel.debug)
+        Else
+            log.fileLoggingLevel = 0
+        End If
+        Return log
+    End Function
+
+    Private Function GetDbConfig(configSource As IniPropsProvider) As MySQLDBConfig
         Dim dbconfig As MySQLDBConfig
         With dbconfig
             Dim env = Environment.GetEnvironmentVariables()
@@ -44,28 +60,40 @@ Module App
                 .database = env("DB_DATABASE")
                 .protocol = "socket"
             Else
-                .host = ini.GetProperty("Database.MySQLHost", "changeme!!")
-                .username = ini.GetProperty("Database.MySQLUser")
-                .password = ini.GetProperty("Database.MySQLPass")
-                .database = ini.GetProperty("Database.MySQLDB")
-                .protocol = ini.GetProperty("Database.MySQLProtocol", "socket")
+                .host = configSource.GetProperty("Database.MySQLHost", "changeme!!")
+                .username = configSource.GetProperty("Database.MySQLUser")
+                .password = configSource.GetProperty("Database.MySQLPass")
+                .database = configSource.GetProperty("Database.MySQLDB")
+                .protocol = configSource.GetProperty("Database.MySQLProtocol", "socket")
             End If
         End With
 
         If dbconfig.host = "changeme!!" Then
-            Throw New Exception("Please configure the scanner first (" & ini.IniName & ")")
+            Throw New Exception("Please configure the scanner first (" & configSource.IniName & ")")
         End If
 
-        dbCtx = New Utt2Context(dbconfig)
+        Return dbconfig
+    End Function
 
+    Private Sub InitScanner(logger As Logger, dbCtx As Utt2Context, master As MasterServerManager)
+        Dim scannerConfig As ServerScannerConfig
+        With scannerConfig
+            .log = logger
+            .dbCtx = dbCtx
+            .masterServerManager = master
+        End With
+        scanner = New Scanner(scannerConfig)
+    End Sub
+
+    Private Sub InitMasterServerManager(logger As Logger, config As IniPropsProvider)
         masterManager = New MasterServerManager()
-        masterManager.log = log
-        masterManager.updateInterval = ini.GetProperty("MasterServer.RefreshIntervalMins", "120") * 60
+        masterManager.log = logger
+        masterManager.updateInterval = config.GetProperty("MasterServer.RefreshIntervalMins", "120") * 60
 
         Dim msIdx As Integer = 0
         Dim masterServerString As String
-        Do While ini.PropertyExists("UBrowserAll.ListFactories[" & msIdx & "]")
-            masterServerString = ini.GetProperty("UBrowserAll.ListFactories[" & msIdx & "]")
+        Do While config.PropertyExists("UBrowserAll.ListFactories[" & msIdx & "]")
+            masterServerString = config.GetProperty("UBrowserAll.ListFactories[" & msIdx & "]")
             If masterServerString <> "" Then
                 masterManager.AddMasterServer(masterServerString)
             End If
@@ -73,8 +101,8 @@ Module App
         Loop
 
         msIdx = 0
-        Do While ini.PropertyExists("XBrowser|XBrowserTabInternet.MasterServer[" & msIdx & "]")
-            masterServerString = ini.GetProperty("XBrowser|XBrowserTabInternet.MasterServer[" & msIdx & "]")
+        Do While config.PropertyExists("XBrowser|XBrowserTabInternet.MasterServer[" & msIdx & "]")
+            masterServerString = config.GetProperty("XBrowser|XBrowserTabInternet.MasterServer[" & msIdx & "]")
             If masterServerString <> "" Then
                 masterManager.AddMasterServer(masterServerString)
             End If
@@ -83,22 +111,12 @@ Module App
 
         masterManager.Refresh()
         masterManager.ThreadLoop()
+    End Sub
 
-        Dim dyncfgDbCtx = New Utt2Context(dbconfig)
-        dyncfg = New DatabasePropsProvider(dyncfgDbCtx).Ns("utt.reaper")
-        dyncfg.SetProperty("configsrc", ini.IniName, True)
-
-        Dim scannerConfig As ServerScannerConfig
-        With scannerConfig
-            .log = log
-            .dbCtx = dbCtx
-            .masterServerManager = masterManager
-        End With
-        scanner = New Scanner(scannerConfig)
-
-        If ini.GetProperty("GSMasterServer.Enabled", "0") = 1 Then
+    Private Sub InitGSMasterServer(dbconfig As MySQLDBConfig, config As IniPropsProvider)
+        If config.GetProperty("GSMasterServer.Enabled", "0") = 1 Then
             log.WriteLine("Enabling GameSpy Master Server...")
-            Dim msPort As Integer = ini.GetProperty("GSMasterServer.Port", "28900")
+            Dim msPort As Integer = config.GetProperty("GSMasterServer.Port", "28900")
             Dim msDbCtx = New Utt2Context(dbconfig)
             master = New GSMasterServer(msPort)
             masterBridge = New GSMasterServerBridge(msDbCtx)
@@ -106,12 +124,6 @@ Module App
             master.LoadGSListFromDict(MasterServerManager.gamespyKeys)
             master.StartServer()
         End If
-
-        scanner.ScannerThread()
-
-        Do
-            Threading.Thread.Sleep(100)
-        Loop
     End Sub
 
     Private Sub master_ClientConnected(client As System.Net.IPEndPoint) Handles master.ClientConnected
@@ -140,7 +152,7 @@ Module App
     End Sub
 
 
-    Private Function GetIniConfig() As IniPropsProvider
+    Private Function GetAppIniProvider() As IniPropsProvider
         Dim bundledConfigProvider = New EmbeddedFileProvider(Assembly.GetExecutingAssembly(), "Naomai.UTT.Indexer")
         Dim bundledConfigFile = bundledConfigProvider.GetFileInfo("ConfigDist.ini")
         Dim bundledConfigStream As Stream = Nothing
